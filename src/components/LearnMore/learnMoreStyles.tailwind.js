42211d8825cd97283c7b4edbdc692fb56c511e31
Shivam-dev-04 <shivamchoudhary@orientaloutsourcing.com>
2025-03-21 16:45:59 +0530

    implemented modifications on landing page

--- src/components/LearnMore/learnMoreStyles.tailwind.js
+++ src/components/LearnMore/learnMoreStyles.tailwind.js
@@ -1,13 +1,13 @@
 export const learnMoreStyles = {
     wrapper: `flex flex-col gap-3 px-6 py-[100px] text-center`,
     heading: `text-[30px] font-[400] font-[family-name:var(--font-bruno)] text-center py-5 gradient-text`,
-    cardContainer: `flex flex-wrap gap-6 md:flex-row pt-6`,
+    cardContainer: `flex flex-wrap gap-6 md:flex-row pt-6 w-full justify-center`,
     lastCardContainer: `flex flex-col space-y-4 min-w-[unset] md:min-w-[370px] md:max-w-[380px]`,
     lastCard: `flex flex-col items-start justify-start  min-h-[340px] 
                 w-full border border-borderColor rounded-[10px] text-gray-300 px-5 !py-0`,
     cardText: `font-[family-name:var(--font-bruno)] grandient-text relative z-20`,
     button: `w-full`,
-    cards: ` min-h-[340px] min-w-[full] md:min-w-[370px] md:max-w-[380px] flex items-start justify-start 
+    cards: ` min-h-[340px] min-w-[full] md:min-w-[370px] md:max-w-[380px] w-[100%] flex items-start justify-start 
                 border border-borderColor rounded-[10px] text-gray-300 p-5 relative overflow-hidden `,
 
 }
